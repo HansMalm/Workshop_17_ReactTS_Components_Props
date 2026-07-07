a71ca5a0fa6c38d62cf454f099c9bff0fce9408e
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-07-07 10:54:57 +0200

    add newsletter and footer
    first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 
 const App = () => {
     return (
         <>
-        <Navbar />
-        <MainContent />
+            <Navbar />
+            <MainContent />
+            <Newsletter />
+            <Footer />
         </>
     );
 };
